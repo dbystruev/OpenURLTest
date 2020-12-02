e4d445d3362ab504ddf506c479c2425ea3ebf4ed
Denis Bystruev <dbystruev@me.com>
2020-12-02 09:09:05 +0300

    Added code, icons, screenshots

--- OpenURLTest/ViewController.swift
+++ OpenURLTest/ViewController.swift
@@ -8,12 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var urlTextFiled: UITextField!
+    
+    // MARK: - Methods
+    func openURL() {
+        guard let url = urlTextFiled.text else { return }
+        if let url = URL(string: url), UIApplication.shared.canOpenURL(url) {
+            urlTextFiled.resignFirstResponder()
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            } else {
+                UIApplication.shared.openURL(url)
+            }
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        urlTextFiled.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        urlTextFiled.delegate = self
+        urlTextFiled.text = "http://ya.ru"
+    }
+    
+    // MARK: - Actions
+    @IBAction func urlTextFieldDidEndOnExit(_ sender: Any) {
+        openURL()
     }
 
-
+    @IBAction func urlButtonTapped(_ sender: UIButton) {
+        openURL()
+    }
 }
 
+// MARK: - UITextFieldDelegate
+extension ViewController: UITextFieldDelegate {    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        openURL()
+        return true
+    }
+}
